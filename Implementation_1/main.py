0d00c1f732a6d685a375fe19e67949a9bb1d95b0
Aria <nothatcreative5@gmail.com>
2021-11-26 19:16:02 +0330

    last

--- Implementation_1/main.py
+++ Implementation_1/main.py
@@ -18,73 +18,43 @@ dRow = [1, 0, -1, 0]
 dCol = [0, 1, 0, -1]
 directions = ['U', 'L', 'D', 'R']
 
-name = input("Please enter the name of the xml file you want to open.\nIf you don't have an xml file you can ignore "
-             "this.\n")
+name = input("Please enter the name of the xml file you want to open.\n")
 
-try:
-    xml = open(name + ".xml", "r")
-    path = xml.read()
-    doc = ET.fromstring(path)
-    row = len(doc)
-    col = len(doc[0])
-    grid = np.zeros((row, col))
-    grid_dir = np.full((row, col), 'N')
-except FileNotFoundError:
-    pass
+xml = open(name + ".xml", "r")
+path = xml.read()
+doc = ET.fromstring(path)
+row = len(doc)
+col = len(doc[0])
+grid, data = np.zeros((row, col)), np.zeros((row, col))
+grid_dir = np.full((row, col), 'N')
 
 pygame.init()
+total_explored = 0
+total_walks = 0
 
 
 def read_from_xml():
+    global x_start, y_start, x_goal, y_goal
     for i in range(row):
         for j in range(col):
             if doc[i][j].text == 'robot':
                 x_start, y_start = i, j
                 grid[i][j] = 1
+                data[i][j] = 1
             elif doc[i][j].text == 'Battery':
+                data[i][j] = 2
                 grid[i][j] = 2
                 x_goal, y_goal = i, j
             elif doc[i][j].text == 'obstacle':
-                grid[i][j] = 3
+                data[i][j] = 3
             else:
                 grid[i][j] = 0
+                data[i][j] = 0
     return grid, (x_start, y_start), (x_goal, y_goal)
 
 
-map_type = int(input("To customize your own map, type 1.\nTo read the map from the xml file, type 2.\n"))
-if map_type == 1:
-    (x_start, y_start) = map(int,
-                             input("Please enter the coordinates for the robot, separated by space.\n").split(' '))
-    (x_goal, y_goal) = map(int, input("Please enter the coordinates for the battery, separated by space.\n").split(' '))
-    (row, col) = map(int, input("Please enter the width and height of your map, separated by space.\n").split(' '))
-    grid = np.zeros((row, col))
-    grid_dir = np.full((row, col), 'N')
-    grid[x_start][y_start] = 1
-    grid[x_goal][y_goal] = 2
-else:
-    grid, (x_start, y_start), (x_goal, y_goal) = read_from_xml()
-
-
-def save_as_XML(name, grid):
-    map = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n"
-    map += "<rows>\n"
-    for i in range(row):
-        map += "<row>\n"
-        for j in range(col):
-            if grid[i][j] == 1:
-                map += "<cell>robot</cell>\n"
-            elif grid[i][j] == 2:
-                map += "<cell>Battery</cell>\n"
-            elif grid[i][j] == 3:
-                map += "<cell>obstacle</cell>\n"
-            else:
-                map += "<cell>empty</cell>\n"
-        map += "</row>\n"
-    map += "</rows>"
-    text_file = open(name, "w")
-    text_file.write(map)
-    text_file.close()
-
+grid, (x_start, y_start), (x_goal, y_goal) = read_from_xml()
+Const_start_x, Const_start_y = x_start, y_start
 
 # This sets the margin between each cell
 MARGIN = 1
@@ -93,8 +63,8 @@ MARGIN = 1
 WIDTH, HEIGHT = 40, 40
 
 if WIDTH * col + (col + 1) * MARGIN > 720 or HEIGHT * row + (row + 1) * MARGIN > 720:
-    HEIGHT = 720 // (row)
-    WIDTH = 720 // (col)
+    HEIGHT = 720 // row
+    WIDTH = 720 // col
 
 frontier = PriorityQueue()
 explored_set = []
@@ -110,14 +80,39 @@ def isValid(x_cord, y_cord, cost):
     return True
 
 
-def print_path():
+def reverse_path():
+    global grid_dir
+    temp_grid = np.full((row, col), 'N')
     cur_x = x_goal
     cur_y = y_goal
-    length = 0
     while cur_x != x_start or cur_y != y_start:
-        length += 1
-        grid[cur_x][cur_y] = 6
-        print(cur_x, cur_y)
+        cur_dir = grid_dir[cur_x][cur_y]
+        print(cur_x,cur_y)
+        if cur_dir == 'U':
+            cur_x -= 1
+            temp_grid[cur_x][cur_y] = 'D'
+        elif cur_dir == 'D':
+            cur_x += 1
+            temp_grid[cur_x][cur_y] = 'U'
+        elif cur_dir == 'R':
+            cur_y += 1
+            temp_grid[cur_x][cur_y] = 'L'
+        else:
+            cur_y -= 1
+            temp_grid[cur_x][cur_y] = 'R'
+    grid[cur_x][cur_y] = 6
+    grid_dir = np.copy(temp_grid)
+
+
+def print_path():
+    global x_start, y_start, grid_dir, explored_set, cost_set, total_walks, total_explored
+    cur_x = x_start
+    cur_y = y_start
+    total_explored += len(explored_set)
+    while cur_x != x_goal or cur_y != y_goal:
+        temp_x, temp_y = cur_x, cur_y
+        total_walks += 1
+        grid[temp_x][temp_y] = 6
         cur_dir = grid_dir[cur_x][cur_y]
         if cur_dir == 'U':
             cur_x -= 1
@@ -127,37 +122,20 @@ def print_path():
             cur_y += 1
         else:
             cur_y -= 1
+        grid[cur_x][cur_y] = 1
+        if data[cur_x][cur_y] == 3:
+            x_start, y_start = temp_x, temp_y
+            grid[cur_x][cur_y] = 3
+            reset(True)
+            return
+
         pygame.event.get()
         Draw()
     grid[x_goal][y_goal] = 2
     grid[x_start][y_start] = 1
-    print(x_start, y_start)
-    print('length = ', length + 1)
-    print('number of explored rooms = ', len(explored_set) + 1)
-
-
-def Search(h, scale):
-    q = frontier.get()
-    x, y, cost = q[1], q[2], q[3]
-    if (x, y) in explored_set:
-        return
-    if x == x_goal and y == y_goal:
-        print_path()
-        frontier.queue.clear()
-        return
-    for i in range(4):
-        x += dRow[i]
-        y += dCol[i]
-        if isValid(x, y, h(x, y) + (cost + 1) * scale):
-            neighbor = ((cost + 1) * scale + h(x, y), x, y, cost + 1)
-            cost_set[x][y] = (cost + 1) * scale + h(x, y)
-            grid_dir[x][y] = directions[i]
-            frontier.put(neighbor)
-            grid[x][y] = 5
-        x = q[1]
-        y = q[2]
-        grid[x][y] = 4
-    explored_set.append((q[1], q[2]))
+    print('length = ', total_walks + 1)
+    print('number of explored rooms = ', total_explored + 1)
+    frontier.queue.clear()
 
 
 def Draw():
@@ -201,13 +179,54 @@ pygame.display.set_caption("Search Algorithms")
 
 # Loop until the user clicks the close button.
 done = False
-search_finish = False
 
 # Used to manage how fast the screen updates
 clock = pygame.time.Clock()
 
+
+def reset(for_search=False):
+    global grid, x_start, y_start, explored_set, grid_dir, cost_set
+    if not for_search:
+        grid = np.zeros((row, col))
+        x_start, y_start = Const_start_x, Const_start_y
+    grid[np.logical_and(grid != 3, grid != 1, grid != 2)] = 0
+    grid[x_start][y_start], grid[x_goal][y_goal] = 1, 2
+    grid_dir = np.full((row, col), 'N')
+    explored_set = []
+    frontier.queue.clear()
+    frontier.put((abs(x_start - x_goal) + abs(y_start - y_goal), x_start, y_start, 0))
+    cost_set = np.full((row, col), -1)
+    cost_set[x_start][y_start] = abs(x_start - x_goal) + abs(y_start - y_goal)
+
+
+def Search(h, scale):
+    q = frontier.get()
+    x, y, cost = q[1], q[2], q[3]
+    if (x, y) in explored_set:
+        return
+    if x == x_goal and y == y_goal:
+        reverse_path()
+        print_path()
+        return
+    for i in range(4):
+        x += dRow[i]
+        y += dCol[i]
+        if isValid(x, y, h(x, y) + (cost + 1) * scale):
+            neighbor = ((cost + 1) * scale + h(x, y), x, y, cost + 1)
+            cost_set[x][y] = (cost + 1) * scale + h(x, y)
+            grid_dir[x][y] = directions[i]
+            frontier.put(neighbor)
+            grid[x][y] = 5
+        x = q[1]
+        y = q[2]
+        grid[x][y] = 4
+    explored_set.append((q[1], q[2]))
+
+
 def search_helper():
-    message = "For A* with random heuristic, type 1.\nFor greedy best search, type 2.\nFor A* with Manhattan heuristic, type 3.\nfor uniform cost search, type 4.\n"
+    global total_walks, total_explored
+    message = "For A* with random heuristic, type 1.\nFor greedy best search, type 2.\nFor A* with Manhattan " \
+              "heuristic, type 3.\nfor uniform cost search, type 4.\n "
     type_of_search = int(input(message))
     if type_of_search == 4:
         h = lambda x, y: 0
@@ -218,13 +237,14 @@ def search_helper():
     elif type_of_search == 2:
         h = lambda x, y: abs(x_goal - x) + abs(y_goal - y)
         scale = 0
-    elif type_of_search == 1:
-        h = lambda x, y: np.random.randint(100,size = 1)[0]
+    else:
+        h = lambda x, y: np.random.randint(100, size=1)[0]
         scale = 1
     while not frontier.empty():
         pygame.event.get()
         Search(h, scale)
         Draw()
+    total_walks, total_explored = 0, 0
 
 
 # -------- Main Program Loop -----------
@@ -239,31 +259,12 @@ while not done:
             new_column = pos[0] // (WIDTH + MARGIN)
             new_row = pos[1] // (HEIGHT + MARGIN)
 
-            if search_finish and new_column == y_start and new_row == x_start:
-                search_finish = False
-                grid_dir = np.full((row, col), 'N')
-                grid[np.logical_and(grid != 3, grid != 1, grid != 2)] = 0
-                grid[x_start, y_start] = 1
-                grid[x_goal][y_goal] = 2
-            elif not search_finish and new_column == y_start and new_row == x_start:
-                search_finish = True
-                explored_set = []
-                cost_set = np.full((row, col), -1)
-                cost_set[x_start][y_start] = abs(x_start - x_goal) + abs(y_start - y_goal)
+            if new_column == y_start and new_row == x_start:
+                reset()
                 search_helper()
-                frontier.put((abs(x_start - x_goal) + abs(y_start - y_goal), x_start, y_start, 0))
-            elif grid[new_row][new_column] == 3:
-                grid[new_row][new_column] = 0
-            elif new_row != x_goal or new_column != y_goal:
-                grid[new_row][new_column] = 3
 
     Draw()
 
 # Be IDLE friendly. If you forget this line, the program will 'hang'
 # on exit.
 pygame.quit()
-
-save = int(input("Type 1 to save the map as an xml file.\nType 2 to skip this process.\n"))
-
-if save == 1:
-    save_as_XML(input("Please enter the file's name.\n") + ".xml", grid)
